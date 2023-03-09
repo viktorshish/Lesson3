472eae4f7c6174dd17c5bdf3f884bda643083d6e
Viktor Shishkalov <viktorshish@gmail.com>
2023-03-09 19:40:08 +0300

    Решение задания - работа с CSV

---
+++ file_csv.py
@@ -0,0 +1,15 @@
+users = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+]
+
+# Запишите содержимое списка словарей в файл в формате csv
+import csv
+
+with open('users.csv', 'w', encoding = 'utf-8') as file:
+    fields = ['name', 'age', 'job']
+    writer = csv.DictWriter(file, fields, delimiter = ';')
+    writer.writeheader()
+    for user in users:
+        writer.writerow(user)
