64c7afb7e2676289204a1b4763b1f0db4c827d36
Viktor Shishkalov <viktorshish@gmail.com>
2023-03-10 11:11:18 +0300

    Поправил отступы и имя файлового объекта

--- file_management.py
+++ file_management.py
@@ -4,7 +4,7 @@
 # Замените точки в тексте на восклицательные знаки
 # Сохраните результат в файл referat2.txt
 
-with open('referat.txt', 'r', encoding = 'utf-8') as file:
+with open('referat.txt', 'r', encoding='utf-8') as file:
     content = file.read()
 string_length = len(content)
 print(f'Длина строки: {string_length}')
@@ -12,5 +12,5 @@ words = content.split()
 print(f'Количество слов в тексте: {len(words)}')
 new_content = content.replace('.', '!')
 
-with open('referat2.txt', 'w', encoding = 'utf-8') as file:
+with open('referat2.txt', 'w', encoding='utf-8') as file:
     file.write(new_content)
