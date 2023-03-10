87ac527d2e2d6e626a119e59df4eac96d844cce5
Viktor Shishkalov <viktorshish@gmail.com>
2023-03-10 11:17:42 +0300

    Поправил отступы у списка словарей, вынес переменую

--- file_csv.py
+++ file_csv.py
@@ -1,14 +1,15 @@
 users = [
-        {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
-        {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
-        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+    {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
+    {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
+    {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
 ]
 
 # Запишите содержимое списка словарей в файл в формате csv
 import csv
 
+fields = ['name', 'age', 'job']
+
 with open('users.csv', 'w', encoding = 'utf-8') as file:
-    fields = ['name', 'age', 'job']
     writer = csv.DictWriter(file, fields, delimiter = ';')
     writer.writeheader()
     for user in users:
