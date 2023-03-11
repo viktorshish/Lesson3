b637d01959695522f29cfc1eea0b930da3fffc9c
Viktor Shishkalov <viktorshish@gmail.com>
2023-03-11 15:55:56 +0300

    Исправил расположение импорта

--- file_csv.py
+++ file_csv.py
@@ -1,11 +1,11 @@
+# Запишите содержимое списка словарей в файл в формате csv
+import csv
+
 users = [
     {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
     {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
     {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
 ]
-
-# Запишите содержимое списка словарей в файл в формате csv
-import csv
 
 fields = ['name', 'age', 'job']
 
