48979af12716af105f78ac8afba01fd306039d3e
Viktor Shishkalov <viktorshish@gmail.com>
2023-03-12 10:52:00 +0300

    Разбор инкапсуляции

--- Inkapsulyacia.py
+++ Inkapsulyacia.py
@@ -3,8 +3,27 @@ class Product:
         self.name = name.strip()
         if len(self.name) < 2:
             raise ValueError('Название товара должно быть больше двух символов') 
-        self.price = price
-        self.stock = stock
-        self.discount = discount
-        self.max_discount = max_discount
+        self.price = abs(float(price))
+        self.stock = abs(int(stock))
+        self.discount = abs(float(discount))
+        self.max_discount = abs(float(max_discount))
+        if self.max_discount > 99:
+            raise ValueError('Слишком большая максимальная скидка скидка')
+        if self.discount > self.max_discount:
+            self.discount = self.max_discount
     
+    def discounted(self):
+        return self.price - self.price * self.discount / 100
+    
+    def sell(self, item_count=1):
+        if item_count > self.stock:
+            raise ValueError('Недостаточно товара на складе')
+        self.stock -= item_count
+
+    def __repr__(self):
+        return f'<product name: {self.name}, price: {self.price}, stock: {self.stock}>'
+    
+product1 = Product('Телефон', 100, 50)
+print(product1)
+product1.sell(5)
+print(product1)
